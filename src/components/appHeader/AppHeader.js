8b3a533f510de847897b4fd7a18d039c6ab0b132
gotikman <vydyvnyk@gmail.com>
2022-05-28 00:58:05 +0300

    fix path problem

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -6,7 +6,7 @@ const AppHeader = () => {
     return (
         <header className="app__header">
             <h1 className="app__title">
-                <Link to="/www-react-api">
+                <Link to="/www-react-api/">
                     <span>Marvel</span> information portal
                 </Link>
             </h1>
@@ -15,7 +15,7 @@ const AppHeader = () => {
                     <li><NavLink
                         end
                         style={({ isActive }) => ({ color: isActive ? '#9f0013' : 'inherit' })}
-                        to="/www-react-api">Characters</NavLink></li>
+                        to="/www-react-api/">Characters</NavLink></li>
 
                     <li><NavLink
                         style={({ isActive }) => ({ color: isActive ? '#9f0013' : 'inherit' })}
